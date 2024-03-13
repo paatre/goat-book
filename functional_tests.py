91001994a98359e5d7f824731aebfda43f78b4e6
Teemu Viikeri <teemu.viikeri@haltu.fi>
2024-03-13 22:39:32 +0200

    First FT speced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,42 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 
-options = Options()
-service = Service(executable_path="/snap/bin/geckodriver")
-browser = webdriver.Firefox(options=options, service=service)
-browser.get("http://localhost:8000")
 
-assert "Congratulations" in browser.title
-print("OK")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        options = Options()
+        service = Service(executable_path="/snap/bin/geckodriver")
+        self.browser = webdriver.Firefox(options=options, service=service)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")
+
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a todo-list
+
+        # There is still a text box inviting her to add another item.
+        # She enteres "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on here list
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
 
