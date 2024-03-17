dfd2f3666668f14afedc75c7e483c9543c193668
Teemu Viikeri <teemu.viikeri@haltu.fi>
2024-03-17 11:42:04 +0200

    WIP: functional test for multiple inputs

--- lists/tests.py
+++ lists/tests.py
@@ -8,3 +8,8 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
+    def test_can_save_a_post_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, "home.html")
+
