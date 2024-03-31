391b1bcdcc98f96da2af186dabe1ecca0d63cecb
Teemu Viikeri <teemu.viikeri@haltu.fi>
2024-04-01 00:14:00 +0300

    Add docstring for home_page view

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import redirect, render
+
 from lists.models import Item
 
 
 def home_page(request):
+    """
+    Includes the list of items for the user.
+    """
     if request.method == "POST":
         Item.objects.create(text=request.POST["item_text"])
         return redirect("/")
 
     items = Item.objects.all()
     return render(request, "home.html", {"items": items})
-
