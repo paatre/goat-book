43395f121a84f290ebfda776d1c8d78232333d38
Teemu Viikeri <teemu.viikeri@haltu.fi>
2024-04-10 23:40:20 +0300

    Prepare Dockerfile for production

--- build-run-container.sh
+++ build-run-container.sh
@@ -1,4 +1,6 @@
 docker build -t superlists . && docker run \
   -p 8888:8888 \
   --mount type=bind,source=./src/db.sqlite3,target=/src/db.sqlite3 \
+  -e DJANGO_SECRET_KEY=sekrit \
+  -e DJANGO_ALLOWED_HOST=localhost \
   -it superlists
--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yuin+1*e2lyx*%ostv*zu!jm89g0(mg&jbk79xk_qpzlxhp1@y"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if "DJANGO_DEBUG_FALSE" in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+else:
+    DEBUG = True
+    SECRET_KEY = "insecure-key-for-dev"
+    ALLOWED_HOSTS = []
 
 
 # Application definition
